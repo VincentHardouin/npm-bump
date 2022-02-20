8b88ac32836e76b76a425dec8d74d2cd64b3e6ee
Vincent Hardouin <vincenthardouin@icloud.com>
2022-02-20 22:27:55 +0100

    fix: tests runner

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while IFS= read -r -d '' file; do
+find ./* -maxdepth 0 -type f -name "*.sh" -not -path "./runner.sh" -print0 | while IFS= read -r -d '' file; do
   bash "$file" || echo "$file" exit 1
-done <   <(find ./* -maxdepth 0 -type f -name "*.sh" -not -path "./runner.sh" -print0)
+done
 
