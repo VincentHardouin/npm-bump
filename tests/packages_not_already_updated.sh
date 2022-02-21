27ff13043ac129eb60956bca3d514ac0198c968a
Vincent Hardouin <vincenthardouin@icloud.com>
2022-02-21 13:13:37 +0100

    fix: test

--- tests/packages_not_already_updated.sh
+++ tests/packages_not_already_updated.sh
@@ -24,8 +24,8 @@ current_dir=$PWD
 directory_under_test='./npm-directory'
 expected_output="Package               Old          New
 --------------------------------------
-lodash            4.17.19      4.17.20"
-expected_commit_message="Bump lodash from 4.17.19 to 4.17.20"
+lodash            4.17.19      4.17.21"
+expected_commit_message="Bump lodash from 4.17.19 to 4.17.21"
 
 cd $directory_under_test || exit
 { npm install --quiet lodash@4.17.19  &> /dev/null; } 2>&1
